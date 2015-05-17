0fc59e61880d1742cc3bef07391965c0d242d7bb
zoski <zoski@free.fr>
2015-05-17 17:05:27 +0200

    Nettoyage

--- Framework/src/fuzzy/NotMinus1.h
+++ Framework/src/fuzzy/NotMinus1.h
@@ -27,39 +27,3 @@ namespace fuzzy {
 
 };
 #endif /* FUZZY_NOTMINUS1_H_ */
-
-//MAMDANIDEFUZZ:
-
-
-//MamdaniDefuzz -> binaire car:
-//systeme de regles et variable de sortie
-//1 construire une forme de maniere recursive sur l'univers de discours
-//2 centre de gravite/max des max/ min des max....
-// mamdani defuzz: evaluate et defuzz
-//defuzz prend en parametre une forme => shape
-//evaluate appelle defeuzz
-//CogDefuzz redefinit defuzz(shape)
-
-//comment tester les fonctions d'apartenance?
-//
-
-//class Evaluator //nom de OUF
-//definir ce qu'est une forme
-//on prend une collection dynamique
-//couple de vecteurs de T::
-
-//typedef pair<std::vector<T>,std::vector<T>> Shape
-////2 trucs:
-//static Shape buildShape(const T& min, same max,s step, left, right)
-//static ostream& printShape(const Shape& ,ostream&)
-//comportement de class
-
-//[.........]
-//[.........]
-
-//quand il y a du typedef, il faut mettre du typename pour l'appeler.
-//accumulate => lib standard
-
-
-//
-
