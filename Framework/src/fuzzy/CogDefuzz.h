f1f2d79313dce5b97ac92536cc57258a17c398b0
zoski <zoski@free.fr>
2015-05-18 17:04:38 +0200

    Pompage code du prof

---
+++ Framework/src/fuzzy/CogDefuzz.h
@@ -0,0 +1,20 @@
+/*
+ * CogDefuzz.h
+ *
+ *  Created on: 17 mai 2015
+ *      Author: gael
+ */
+
+#ifndef FUZZY_COGDEFUZZ_H_
+#define FUZZY_COGDEFUZZ_H_
+
+/*
+ * Permet de calculer le résultat final (centre de gravité, moyenne, max ect.
+ *
+ */
+
+namespace fuzzy {
+
+}
+
+#endif /* FUZZY_COGDEFUZZ_H_ */
---
+++ Framework/src/fuzzy/Evaluator.h
@@ -0,0 +1,63 @@
+/*
+ * Evaluator.h
+ *
+ *  Created on: 18 mai 2015
+ *      Author: gael
+ *
+ *      D'après le .pdf du cours page 36 à 37
+ */
+
+#ifndef CORE_EVALUATOR_H_
+#define CORE_EVALUATOR_H_
+
+#include <vector>
+
+namespace core {
+	template <class T>
+	class Evaluator {
+	public:
+		class EvalFunc {
+		public:
+			virtual T operator () (const T&) = 0;
+		};
+
+		typedef std::pair< std::vector<T>, std::vector<T> > Shape;
+
+		static Shape BuildShape(const T& min, const T& max, const T& step, ValueModel<T>*, Expression<T>*);
+		static std::ostream& PrintShape(std::ostream& o, const Shape& s);
+
+	};
+
+	template <class T>
+	typename Evaluator<T>::Shape Evaluator<T>::BuildShape(const T& min, const T& max, const T& step, ValueModel<T>* v, Expression<T>* e) {
+		std::vector<T> x , y;
+		T mem = v-> Evaluate();
+
+		for(T i = min ; i<= max ; i += step) {
+			v->setValue(i);
+			y.push_back(i);
+			x.push_back(e->Evaluate());
+		}
+		return Shape(x, y);
+	}
+
+	template <class T>
+	std::ostream& Evaluator<T>::PrintShape(std::ostream& os, const Shape& s) {
+		os << '[';
+		typename std::vector<T>::const_iterator it = s.first.begin();
+		for(; it!=s.first.end(); ++it) {
+			os << *it << ' ';
+		}
+		os << ']';
+		os << std::endl;
+		os << '[';
+		it = s.second.begin();
+		for(; it!=s.second.end(); ++it) {
+			os << *it << ' ';
+		}
+		os << ']';
+		return os;
+	}
+}
+
+#endif /* CORE_EVALUATOR_H_ */
